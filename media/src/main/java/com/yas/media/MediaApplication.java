d5f67d6385e2fcd714f14060455f605624099cf7
Melyenn <thiduyen310@gmail.com>
2026-05-15 20:40:43 +0700

    test media

--- media/src/main/java/com/yas/media/MediaApplication.java
+++ media/src/main/java/com/yas/media/MediaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"com.yas.media", "com.yas.commonlibrary"})
-@EnableConfigurationProperties({YasConfig.class, CorsConfig.class})
+@SpringBootApplication(scanBasePackages = { "com.yas.media", "com.yas.commonlibrary" })
+@EnableConfigurationProperties({ YasConfig.class, CorsConfig.class })
 public class MediaApplication {
 
     public static void main(String[] args) {
